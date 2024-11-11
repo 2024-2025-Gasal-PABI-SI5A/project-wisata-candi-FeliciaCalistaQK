4029a235d469f3652a42037e0b21579c59deb048
FeliciaCalistaQK <161106015+FeliciaCalistaQK@users.noreply.github.com>
2024-11-11 20:38:20 +0700

    wisatacandi

---
+++ wisatacandi/lib/screens/detail_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:wisatacandi/models/candi.dart';
+
+class DetailScreen extends StatelessWidget{
+  final Candi candi;
+
+  const DetailScreen({super.key,required this.candi});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          Stack(
+            children: [
+
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
